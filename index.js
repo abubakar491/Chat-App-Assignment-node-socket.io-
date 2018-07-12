15e3c4509d790608886e1d085f9b02c7bdf4fb63
Abubakar Akram <pipsonion2010@gmail.com>
2018-07-12 20:00:37 +0500

    Final Without Database

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-var express = require('express');
-var socket = require('socket.io');
+const express = require('express');
+const socket = require('socket.io');
+
 
 // App setup
 var app = express();
 var server = app.listen(3000, function(){
-    console.log('listening for requests on port 000,');
+    console.log('listening for requests on port 3000,');
 });
 
 // Static files
@@ -16,13 +17,13 @@ io.on('connection', (socket) => {
 
     console.log('made socket connection', socket.id);
 
-    // Handle chat event
     socket.on('chat', function(data){
-        console.log(data);
         io.emit('chat', data);
+
+        //Insert Into Database
     });
 
-    // Handle typing event
+
     socket.on('typing', function(data){
         socket.broadcast.emit('typing', data);
     });
--- public/chat.js
+++ public/chat.js
@@ -1,28 +1,28 @@
 var socket = io.connect('http://localhost:3000');
 
 var message = document.getElementById('message'),
-      handle = document.getElementById('handle'),
       send = document.getElementById('send'),
       output = document.getElementById('output'),
-      feedback = document.getElementById('feedback');
+      feedback = document.getElementById('feedback'),
+      username = document.getElementById('username');
 
 // Emit events
 send.addEventListener('click', function(){
     socket.emit('chat', {
         message: message.value,
-        handle: handle.value
+        username:username.value
     });
     message.value = "";
 });
 
 message.addEventListener('keypress', function(){
-    socket.emit('typing', handle.value);
+    socket.emit('typing', username.value);
 })
 
 // Listen for events
 socket.on('chat', function(data){
     feedback.innerHTML = '';
-    output.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.message + '</p>';
+    output.innerHTML += '<p><strong>' + data.username + ': </strong>' + data.message + '</p>';
 });
 
 socket.on('typing', function(data){
